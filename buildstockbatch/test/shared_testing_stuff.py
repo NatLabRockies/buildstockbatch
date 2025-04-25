445d91052f2422190b5942764e4d408bd68a1d9a
Rajendra Adhikari <rajendraadhikari.ee@gmail.com>
2025-04-24 22:57:11 -0500

    Fix resstock dir name

--- buildstockbatch/test/shared_testing_stuff.py
+++ buildstockbatch/test/shared_testing_stuff.py
@@ -8,7 +8,7 @@ import pytest
 resstock_directory = pathlib.Path(
     os.environ.get(
         "RESSTOCK_DIR",
-        pathlib.Path(__file__).resolve().parent.parent.parent.parent / "resstock2025",
+        pathlib.Path(__file__).resolve().parent.parent.parent.parent / "resstock",
     )
 )
 resstock_required = pytest.mark.skipif(not resstock_directory.exists(), reason="ResStock checkout is not found")
