a117c74e1c86ddc543d39b248c0e4d6f67f54d07
Joe Robertson <joseph.robertson@nrel.gov>
2026-06-15 10:33:45 -0700

    Enable optional segment arguments in project yml file.

--- buildstockbatch/sampler/residential_stratified.py
+++ buildstockbatch/sampler/residential_stratified.py
@@ -16,6 +16,7 @@ import shutil
 import subprocess
 import sys
 import time
+import yaml
 
 from .base import BuildStockSampler
 from buildstockbatch.exc import ValidationError
@@ -24,7 +25,21 @@ logger = logging.getLogger(__name__)
 
 
 class ResidentialStratifiedSampler(BuildStockSampler):
-    def __init__(self, parent, n_datapoints):
+    def __init__(
+        self,
+        parent,
+        n_datapoints,
+        segment_vars=[
+            "Federal Poverty Level",
+            "Geometry Floor Area Bin",
+            "Geometry Building Type RECS",
+            "Vintage",
+            "Heating Fuel",
+            "Sampling Region",
+        ],
+        segment_selection_sample_size=10000000,
+        num_samples_per_segment=8,
+    ):
         """Residential Stratified Sampler
 
         :param parent: BuildStockBatchBase object
@@ -35,6 +50,12 @@ class ResidentialStratifiedSampler(BuildStockSampler):
         super().__init__(parent)
         self.validate_args(self.parent().project_filename, n_datapoints=n_datapoints)
         self.n_datapoints = n_datapoints
+        self.sampler_config = self.create_sampler_config(
+            os.path.dirname(self.parent().project_filename),
+            segment_vars,
+            segment_selection_sample_size,
+            num_samples_per_segment,
+        )
 
     @classmethod
     def validate_args(cls, project_filename, **kw):
@@ -46,11 +67,28 @@ class ResidentialStratifiedSampler(BuildStockSampler):
                     raise ValidationError("n_datapoints needs to be an integer")
                 if v <= 0:
                     raise ValidationError("n_datapoints need to be >= 1")
+            elif k == "segment_vars":
+                pass
+            elif k == "segment_selection_sample_size":
+                pass
+            elif k == "num_samples_per_segment":
+                pass
             else:
                 raise ValidationError(f"Unknown argument for sampler: {k}")
         if len(expected_args) > 0:
             raise ValidationError("The following sampler arguments are required: " + ", ".join(expected_args))
         return True
+
+    @classmethod
+    def create_sampler_config(self, folderpath, segment_vars, segment_selection_sample_size, num_samples_per_segment):
+        data = {}
+        data["segment_vars"] = segment_vars
+        data["segment_selection_sample_size"] = segment_selection_sample_size
+        data["num_samples_per_segment"] = num_samples_per_segment
+        filename = pathlib.Path(folderpath) / "sampler_config.yaml"
+        with open(filename, "w") as file:
+            yaml.dump(data, file)
+        return filename
 
     def _run_sampling_docker(self):
         docker_client = docker.DockerClient.from_env()
@@ -122,6 +160,8 @@ class ResidentialStratifiedSampler(BuildStockSampler):
                 self.cfg["project_directory"],
                 "-n",
                 str(self.n_datapoints),
+                "-c",
+                self.sampler_config,
                 "-o",
                 "buildstock.csv",
             ],
@@ -135,4 +175,7 @@ class ResidentialStratifiedSampler(BuildStockSampler):
             pathlib.Path(self.buildstock_dir, "resources", "buildstock.csv"),
             destination_filename,
         )
+        config_filename = pathlib.Path(self.sampler_config)
+        if config_filename.exists():
+            os.remove(config_filename)
         return destination_filename
